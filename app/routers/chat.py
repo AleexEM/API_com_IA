68fa7369ce394e7e58db32f5fac0d1ee19631ab7
AleexEM <alexmeurer3@gmail.com>
2025-08-01 21:48:09 -0300

    atualizaçoes no chat das routers

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from app.agents.ex_2.response_format import gerar_resposta
 from typing import Optional
 import uuid
+from mongodb_database import mongodb_client
 
 router = APIRouter(prefix="/chat", tags=["Chat"])
 class ChatQuestion(BaseModel):
@@ -16,13 +17,25 @@ class ChatResponse(BaseModel):
 
 @router.post("/", response_model=ChatResponse, status_code=status.HTTP_200_OK, summary="Enviar nova Mensagem de Consulta")
 def enviar_mensagem(pergunta: ChatQuestion):
+    chat_sessions = mongodb_client["Cluster0"].chat_sessions
+    conversation_history = []
+    if pergunta.session:
+        session = pergunta.session
+        conversation_history = list(chat_sessions.find({"session": session}))
+        print(conversation_history)
+    else:
+        session = str(uuid.uuid4())
 
-    session = str(uuid.uuid4())
+        # Inserindo no MongoDB o registro da conversa/interação
 
-    resposta = gerar_resposta(pergunta.mensagem)
+    resposta, consulta_sql = gerar_resposta(pergunta.mensagem, historico=conversation_history)
 
-    return {
+    interacao = {
         "pergunta": pergunta.mensagem,
         "resposta": resposta,
         "session": session
     }
+
+    chat_sessions.insert_one(interacao | {"consulta_sql": consulta_sql})
+
+    return interacao
